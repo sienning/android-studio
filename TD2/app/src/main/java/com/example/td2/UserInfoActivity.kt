6e03df522f30b43688e51687efcc8e11f54d5a9b
Léna ENG <55486985+sienning@users.noreply.github.com>
2019-12-30 11:54:24 +0100

    ajout de l'ouverture de la caméra

--- TD2/app/src/main/java/com/example/td2/UserInfoActivity.kt
+++ TD2/app/src/main/java/com/example/td2/UserInfoActivity.kt
@@ -2,15 +2,28 @@ package com.example.td2
 
 import android.Manifest
 import android.app.AlertDialog
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_user_info.*
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 
 class UserInfoActivity : AppCompatActivity() {
+
+    val CAMERA_REQUEST_CODE = 2001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,6 +71,8 @@ class UserInfoActivity : AppCompatActivity() {
 
     private fun openCamera() {
         // On va utiliser un Intent implicite
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
     }
 
     companion object {
@@ -65,8 +80,44 @@ class UserInfoActivity : AppCompatActivity() {
     }
 
 
-    /*override fun onRequestPermissionsResult() {
-        if (requestCode == CAMERA_PERMISSION_CODE && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED)
+    fun onRequestPermissionsResult(requestCode: Int, grantResults: Array<Int>) {
+        if (requestCode == CAMERA_PERMISSION_CODE && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED){
+            openCamera()
+        } else {
+            Toast.makeText(this, "Si vous refusez, on peux pas prendre de photo ! 😢", Toast.LENGTH_LONG).show()
+        }
+    }
 
+    /*private fun handlePhotoTaken(data: Intent?) {
+        val image = data?.extras?.get("data") as? Bitmap
+        // Afficher l'image ici
+
+
+        val imageBody = imageToBody(image)
+        // Plus tard, on l'enverra au serveur
+    }
+
+    // Celle ci n'est pas très intéressante à lire
+// En gros, elle lit le fichier et le prépare pour l'envoi HTTP
+    private fun imageToBody(image: Bitmap?): MultipartBody.Part {
+        val f = File(cacheDir, "tmpfile.jpg")
+        f.createNewFile()
+        try {
+            val fos = FileOutputStream(f)
+            image?.compress(Bitmap.CompressFormat.PNG, 100, fos)
+            fos.flush()
+            fos.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        val body = RequestBody.create(MediaType.parse("image/png"), f)
+        return MultipartBody.Part.createFormData("avatar", f.path, body)
+    }
+
+    fun onActivityResult(){
+
+        handlePhotoTaken()
     }*/
 }
